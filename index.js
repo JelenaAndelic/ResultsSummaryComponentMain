8c998433e696bb2880e25ef377581218a9b980c9
Jelena Andjelic <jelena.andjelic.gr@gmail.com>
2023-02-25 18:30:09 +0100

    Mobile responsive design

---
+++ index.js
@@ -0,0 +1,22 @@
+const summaryResults = [];
+
+fetch("./data.json")
+  .then((response) => response.json())
+  .then((results) => summaryResults.push(...results));
+
+window.onload = function (e) {
+  //   e.preventDefault();
+  const listItems = summaryResults
+    .map((item) => {
+      return `
+    <li>
+    <span class="category"><img src="${item.icon}"></img>
+    ${item.category}</span>
+    <span class="score"><span class="bold">${item.score}</span> / 100</span>
+    </li>
+    `;
+    })
+    .join("");
+
+  document.getElementById("items").innerHTML = listItems;
+};
